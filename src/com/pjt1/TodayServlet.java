d7600ddcec4f2d9eba001125d5ca011043ac3ffb
kdpark <kdpark1623@gmail.com>
2020-04-24 21:15:23 +0900

    간단한 서블릿 자기소개 페이지 만들기

---
+++ src/com/pjt1/TodayServlet.java
@@ -0,0 +1,41 @@
+package com.pjt1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TodayServlet
+ */
+@WebServlet(name = "today", urlPatterns = { "/today" })
+public class TodayServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TodayServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		SimpleDateFormat format = new SimpleDateFormat ( "yyyy/MM/dd HH:mm");
+		Date date = new Date();
+		String time = format.format(date);
+		out.print("현재시간 : "+time);
+	}
+}
